136925b8dd3e829a934a0a2262d1b3c9a2f31388
Saurav Savarn <sauravsavarn@gmail.com>
2023-07-02 21:03:48 +0530

    Changes: 	1. commented original app.js file as backup to take reference 	   the work done in master branch 	2. Added logic in app.js. What we done in this block of code is connected our frontend to the Hardhat network            that is running locally on our machine. And we have done that using Metamask and the account that was created wh           en we started the local Hardhat network.

--- frontend/app-BKP.js
+++ frontend/app-BKP.js
@@ -1,29 +1,7 @@
 App = {
     init: async function() {
         console.log("init is called");
-
-        //#############
-        //What we done in this block of code is connected our frontend to the Hardhat network
-        //that is running locally on our machine. And we have done that using Metamask and the 
-        //account that was created when we started the local Hardhat network. 
-        /** ** Now starting to write some of #Web3 specific code, that will connect our #Wallet
-         *     to the Frontend,
-         *  ** */
         
-        //Initializing our provider
-        const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
-
-        //to send the transaction here, we needs to sign the transaction thus needs to requests the accounts
-        await provider.send("eth_requestAccounts", []);
-        const signer = provider.getSigner();
-        let userAddress = await signer.getAddress();
-
-        //
-        document.getElementById("wallet").innerText = "Your wallet address is: " + userAddress;
-
-        // NOTE : below resourceAddress was printed when deployed the Contract BasicMarketplace locally to hardhat.
-        const resourceAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-        //#############
 
         $.getJSON("../sampleData.json", function(data){
             console.log(data);
